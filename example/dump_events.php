55cf1563cf63efc3ed54cced160bd08b42b505cd
fazi <kacper.rowinski@gmail.com>
2016-02-19 00:31:05 +0100

    refactoring

--- example/dump_events.php
+++ example/dump_events.php
@@ -1,31 +1,15 @@
 <?php
 error_reporting(E_ALL);
 date_default_timezone_set('UTC');
-
 ini_set('memory_limit', '8M');
 
 include __DIR__ . '/../vendor/autoload.php';
 
-function dpack($pack)
-{
-    $field = bin2hex($pack);
-    $field = chunk_split($field, 2, "\\x");
-    $field = "\\x" . substr($field, 0, -2);
-    echo $field . PHP_EOL;
-}
-
-use MySQLReplication\Definitions\ConstEventType;
 use MySQLReplication\Service\BinLogStream;
 use MySQLReplication\Config\Config;
 
-// $gtid = '', $slave_id = '', $logFile = '', $logPos = '', array $ignoredEvents = [], array $onlyTables = [], $onlyDatabases = []
 $binLogStream = new BinLogStream(
-    new Config('root', '192.168.1.100', 3306, 'root'),
-    '9b1c8d18-2a76-11e5-a26b-000c2976f3f3:1-99428',
-    '',
-    '',
-    '',
-    [ ]
+    new Config('root', '192.168.1.100', 3306, 'root')
 );
 while (1)
 {
