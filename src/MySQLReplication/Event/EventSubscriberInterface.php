40a27a8dda4a522d4ec33e70fc8f8faf04e7e4da
Sergey Rakhmanchuk <sergey.rakhmanchuk@supermetrics.com>
2024-06-04 09:58:25 +0300

    Add EventSubscriberInterface to MySQLReplication
    A new file, EventSubscriberInterface.php, was added to the MySQLReplication\Event namespace. This interface extends the Symfony EventSubscriberInterface, providing additional functionality for event management within the MySQLReplication library.

---
+++ src/MySQLReplication/Event/EventSubscriberInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MySQLReplication\Event;
+
+interface EventSubscriberInterface extends \Symfony\Component\EventDispatcher\EventSubscriberInterface
+{
+
+}
